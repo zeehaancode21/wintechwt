7c931727f754f319aa337f52344d8eda1b1c3f61
zeehaancode21 <zeehaancode@gmail.com>
2026-07-12 13:06:14 +0530

    update the wintech

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -72,7 +72,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:card", content: "summary_large_image" },
     ],
     links: [
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
